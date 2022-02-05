12fa74d1d91567183adb878c9390adbe808f62c1
Daria Abushenko <2009add@gmail.com>
2022-02-05 19:16:45 +0500

    favicon and font fix

---
+++ pages/_document.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Html, Head, Main, NextScript } from "next/document";
+
+const Document = () => {
+  return (
+    <Html>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Exo+2:ital,wght@0,100;0,200;0,300;0,400;0,600;0,700;1,200&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
+
+export default Document;
