7acf9da43f37f4b7eb13cb6b7ffc6785f5c16978
Daria Abushenko <2009add@gmail.com>
2022-06-22 23:44:19 +0500

    toast styles fix (global style.css added)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import '../styles/globals.css';
+import 'react-toastify/dist/ReactToastify.css';
 import type { AppProps } from 'next/app';
 import { RefsProvider } from '../context/refs';
 import { SpyCheckProvider } from '../context/spyCheck';
