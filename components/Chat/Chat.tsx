c1d3fb85d45b6814c9a1a87bc53e015a42d5ed13
abshnko <2009add@gmail.com>
2025-01-08 19:12:02 +0300

    fix(chat): made appearing answers not repeat

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -3,6 +3,7 @@ import s from './Chat.module.scss';
 import InputComponent from '../Input/Input';
 import { chatAnswers } from '../../data/chatAnswers';
 import SpeechBubble from '../Speechbubble/Speechbubble';
+import useLocalStorage from 'use-local-storage';
 
 function randomIntFromInterval(min: number, max: number) {
     return Math.floor(Math.random() * (max - min + 1) + min)
@@ -12,19 +13,25 @@ export const Chat = () => {
     const [message, setMessage] = useState("");
     const [answers, setAnswers] = useState<string[]>([]);
     const [questions, setQuestions] = useState<string[]>([]);
+    const [shownAnswers, setShownAnswers] = useLocalStorage<number[]>('shown answers', [])
     const [id, setId] = useState(() => {
-        const id = randomIntFromInterval(chatAnswers[0].id, chatAnswers[chatAnswers.length - 1].id)
+        let id = randomIntFromInterval(chatAnswers[0].id, chatAnswers[chatAnswers.length - 1].id)
+        while (shownAnswers.length < chatAnswers.length && shownAnswers.includes(id)) {
+            id = randomIntFromInterval(chatAnswers[0].id, chatAnswers[chatAnswers.length - 1].id)
+        }
         return id
     })
     const chatContainerRef = useRef<HTMLDivElement | null>(null);
 
     const handleSend = () => {
-        console.log("Message sent:", message);
         setMessage("");
         setQuestions((prev) => [...prev, message])
         setAnswers((prev) => [...prev, chatAnswers.find((ans) => ans.id === id)?.text || ''])
-        const id = randomIntFromInterval(chatAnswers[0].id, chatAnswers[chatAnswers.length - 1].id)
-        setId(id)
+        let newId = randomIntFromInterval(chatAnswers[0].id, chatAnswers[chatAnswers.length - 1].id)
+        while (newId === id || (shownAnswers.length < chatAnswers.length && shownAnswers.includes(newId))) {
+            newId = randomIntFromInterval(chatAnswers[0].id, chatAnswers[chatAnswers.length - 1].id)
+        }
+        setId(newId)
     };
 
     // Scroll to the bottom whenever messages change
@@ -35,6 +42,14 @@ export const Chat = () => {
         }
     }, [answers]);
 
+      useEffect(() => {
+        if (shownAnswers.length < chatAnswers.length) {
+          setShownAnswers([...shownAnswers, id])
+        } else {
+          setShownAnswers([id])
+        }
+      }, [id])
+
     return (
         <>
             <div className={s.container}>
@@ -43,7 +58,7 @@ export const Chat = () => {
                 ) : (
                     <div className={s.messages} ref={chatContainerRef}>
                         {questions.map((q, i) => {
-                            return (<div key={i} style={{display: 'flex', flexDirection:'column'}}>
+                            return (<div key={i} style={{ display: 'flex', flexDirection: 'column' }}>
                                 <SpeechBubble text={q} isAnswer={true} />
                                 <SpeechBubble text={answers[i]} />
                             </div>)
